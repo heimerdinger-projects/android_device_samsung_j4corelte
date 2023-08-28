ec90f82855e72ba37d327c205a8938c1a8872069
Vhmit <viktor.9630@hotmail.com>
2023-08-28 09:11:40 -0300

    j4corelte: Configure architecture and platform flags
    Change-Id: I6ae6cad486eab4e839a9f91480e3a4d8ee811436

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,8 +10,29 @@ TARGET_VENDOR := samsung
 # Inherit from common msm8917-common
 -include device/samsung/msm8917-common/BoardConfigCommon.mk
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := j4corelte, j4coreltejx
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := RG13A002KU
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := msm8937
+
+# SurfaceFlinger
+TARGET_USE_QCOM_SURFACEFLINGER := true
+
+# Timeservice
+BOARD_USES_QC_TIME_SERVICES := true
 
 # Inherit from the proprietary version
 -include vendor/samsung/j4corelte/BoardConfigVendor.mk
